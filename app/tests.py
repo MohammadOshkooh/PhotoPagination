175d0b53262ec973930d7f0723dddc8c5faa5755
mohammadoshkooh <mohammaadoshkooh@gmail.com>
2023-08-09 13:10:19 +0330

    api test for pagination view

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .models import Photo
 
-# Create your tests here.
+
+class PhotoListAPITest(APITestCase):
+
+    def setUp(self):
+        for i in range(10):
+            Photo.objects.create(photo=f'media/photo/{i}.jpg')
+
+    def test_pagination(self):
+        url = reverse('photo-list')
+        response = self.client.get(url)
+
+        # check Meta
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(200, response.data['results']['Meta']['statusCode'])
+        self.assertEqual('success', response.data['results']['Meta']['error'])
+
+        # Test the number of items per page
+        self.assertEqual(len(response.data['results']['Data']), 5)
+
+        # Check values in pagination links
+        self.assertEqual('http://testserver/photo/?page=2', response.data['next'])
+        self.assertEqual(None, response.data['previous'])
+        self.assertEqual(response.data['count'], 10)
--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ImageListView
 
 urlpatterns = [
-    path('photo/', ImageListView.as_view())
+    path('photo/', ImageListView.as_view(), name='photo-list')
 ]

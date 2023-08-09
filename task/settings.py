ffbe0084006ed3dcfd476597c42e06e1b655b6c5
mohammadoshkooh <mohammaadoshkooh@gmail.com>
2023-08-09 13:17:21 +0330

    add .env

--- task/settings.py
+++ task/settings.py
@@ -18,11 +18,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h#axk-=nko-&l0^zt8wl^9yg#3s--ab0=xvqlrid8#)f4iz16d'
+import os
+from dotenv import load_dotenv
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# Load variables from .env file
+load_dotenv()
+
+# Set Django settings from environment variables
+DEBUG = os.getenv('DEBUG')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 ALLOWED_HOSTS = []
 
